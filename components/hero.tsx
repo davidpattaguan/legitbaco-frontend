87280c961893da789407282e50f66d72e7f3fb28
davidpattaguanAGPH <105701295+davidpattaguanAGPH@users.noreply.github.com>
2023-08-25 18:57:29 +0800

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -89,6 +89,49 @@ export function Hero() {
         </Button>
       </div> */}
       </div>
+<<<<<<< Updated upstream
+=======
+
+      <section
+        id="categories"
+        aria-labelledby="categories-heading"
+        className="space-y-6 py-6 md:pt-10  container mx-auto "
+      >
+        <div className="mx-auto flex max-w-[58rem]  flex-col items-center space-y-4 text-center">
+          <h2 className="text-3xl font-bold leading-[1.1] sm:text-3xl md:text-5xl">
+            Categories
+          </h2>
+        </div>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+          {productCategories.map((category) => (
+            <Link
+              aria-label={`Go to ${category.title}`}
+              key={category.title}
+              href={`/categories/${category.title}`}
+            >
+              <div className="group relative overflow-hidden rounded-md">
+                <AspectRatio ratio={4 / 5}>
+                  <div className="absolute inset-0 z-10 bg-black/60 transition-colors group-hover:bg-black/70" />
+                  <Image
+                    src={category.image}
+                    alt={category.title}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    fill
+                    className="object-cover transition-transform group-hover:scale-105"
+                    priority
+                  />
+                </AspectRatio>
+                <div className="absolute inset-0 z-20 flex items-center justify-center">
+                  <h3 className="text-3xl font-medium capitalize text-slate-100 md:text-2xl">
+                    {category.title}
+                  </h3>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+>>>>>>> Stashed changes
     </>
   )
 }
