0c3fff64ad1c3f8afb6afb84dbe9a65adc4d1a46
davidpattaguanAGPH <105701295+davidpattaguanAGPH@users.noreply.github.com>
2023-08-28 21:25:35 +0800

    uo

--- app/(auth)/verify-token/[token]/page.tsx
+++ app/(auth)/verify-token/[token]/page.tsx
@@ -1,4 +1,6 @@
+import { verify } from "crypto"
 import { type Metadata } from "next"
+import { notFound } from "next/navigation"
 
 import {
   Card,
@@ -7,6 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { ErrorCard } from "@/components/error-card"
 import { ChangePasswordForm } from "@/components/forms/change-password-form"
 import { ResetPasswordForm } from "@/components/forms/reset-password-form"
 import { Shell } from "@/components/shell/shell"
@@ -16,18 +19,67 @@ export const metadata: Metadata = {
   description: "Enter your email to reset your password",
 }
 
-export default function ResetPasswordPage() {
+async function verifyToken(token: string) {
+  try {
+    const response = await fetch(
+      `${process.env.BACKEND_URL}/verify-reset-token`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          token,
+        }),
+      }
+    )
+
+    const data = await response.json()
+
+    return data
+  } catch (error) {
+    return notFound()
+  }
+}
+
+export default async function ResetPasswordPage({
+  params,
+}: {
+  params: { token: string }
+}) {
+  const isTokenVerified = await verifyToken(params.token)
+
+  console.log(isTokenVerified)
+
+  if (isTokenVerified.message == "Invalid or expired token") {
+    console.log("INVALID TOKEN")
+  }
+
   return (
-    <Shell className="max-w-lg">
-      <Card>
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl">Enter New Password</CardTitle>
-          <CardDescription>Enter your new password.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ChangePasswordForm />
-        </CardContent>
-      </Card>
-    </Shell>
+    <>
+      {" "}
+      <Shell className="max-w-lg">
+        {isTokenVerified.message == "Invalid or expired token" ? (
+          <>
+            <ErrorCard
+              retryLink="/forgot-password"
+              retryLinkText="Back to forgot password"
+              title="Reset Password Error!"
+              description="Reset Token Expired. Please reset password again"
+            />
+          </>
+        ) : (
+          <>
+            {" "}
+            <Card>
+              <CardHeader className="space-y-1">
+                <CardTitle className="text-2xl">Enter New Password</CardTitle>
+                <CardDescription>Enter your new password.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ChangePasswordForm />
+              </CardContent>
+            </Card>
+          </>
+        )}
+      </Shell>{" "}
+    </>
   )
 }
